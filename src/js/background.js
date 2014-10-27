90efe7085c9e650817cc94d70b3ee0bbc343bcb1
Greduan <eduanlavaque@gmail.com>
2014-10-26 18:33:12 -0600

    Make tab groups

--- src/js/background.js
+++ src/js/background.js
@@ -3,12 +3,32 @@
 (function () {
     'use strict';
 
-    var ripTabs = function (tabsArr) {
-        var tabsInfo = [];
+    function makeTabGroup(tabsArr) {
+        var tabsInfo = [],
+            tabGroup = { groupDate: new Date() };
 
         tabsArr.forEach(function (tab) {
+            tabsInfo.push({
+                tabTitle: tab.title,
+                tabUrl: tab.url
+            });
         });
-    };
+
+        tabGroup.tabs = tabsInfo;
+
+        return tabGroup;
+    }
+
+    function filterTabGroup(tabGroup) {
+        return tabGroup;
+    }
+
+    function ripTabs(tabsArr) {
+        var tabGroup = makeTabGroup(tabsArr),
+            cleanTabGroup = filterTabGroup(tabGroup);
+
+        console.log(cleanTabGroup);
+    }
 
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         switch (request.action) {
