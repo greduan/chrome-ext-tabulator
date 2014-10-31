d934ae19108fe1cba40358c2ca708238052362e1
Greduan <eduanlavaque@gmail.com>
2014-10-31 16:37:29 -0600

    Gonna start using popup again, seems the best way

--- js/tabsshouldrest.js
+++ js/tabsshouldrest.js
@@ -8,7 +8,7 @@
 
     // from the array of Tab objects it makes an object with date and the array
     function makeTabGroup(tabsArr) {
-        var tabGroup = { date: new Date() };
+        var tabGroup = { date: new Date('YYYY-MM-DD at hh:mm:dd') };
 
         tabGroup.tabs = tabsArr;
 
@@ -31,6 +31,7 @@
         }
     }
 
+    // close all the tabs found in the array of Tab objects
     function closeTabs(tabsArr) {
         var tabsToClose = [];
 
@@ -49,30 +50,35 @@
         saveTabGroup(cleanTabGroup);
     }
 
-    // listen for when the browser action is clicked
-    chrome.browserAction.onClicked.addListener(function (tab) {
-        chrome.tabs.query({ currentWindow: true }, function (tabsArr) {
-            ripTabs(tabsArr); // store current tab group as a new tab group
-            // open the background page with the groups list
-            chrome.tabs.create({
-                url: chrome.extension.getURL('tabsshouldrest.html'),
-                windowId: chrome.windows.WINDOW_ID_CURRENT
-            });
-            closeTabs(tabsArr);
+    // open the background page with the groups list in the current window
+    // opening it now so window doesn't close because of lack of tabs
+    function openBackgroundPage() {        
+        chrome.tabs.create({
+            url: chrome.extension.getURL('tabsshouldrest.html'),
+            windowId: chrome.windows.WINDOW_ID_CURRENT
         });
-    });
+    }
+
+    // listen for when the browser action is clicked
+    // chrome.browserAction.onClicked.addListener(function (tab) {
+    //     chrome.tabs.query({ currentWindow: true }, function (tabsArr) {
+    //         ripTabs(tabsArr);
+    //         openBackgroundPage();
+    //         closeTabs(tabsArr);
+    //     });
+    // });
 
     // make HTML with the groups
     JSON.parse(localStorage.tabGroups).forEach(function (group, i) {
-        groupsDiv.add("div.group");
+        groupsDiv.add('div.group'); // one div per group
 
-        curGroupDiv = groupsDiv.query(".group").only(i);
-        curGroupDiv.add("h2").textContent = group.date;
-        curGroupDiv.each(function (el) {
-            el.add("ul>li*" + group.tabs.length);
-            el.ul.li.each(function (el, i) {
-                el.add('a[href="' + group.tabs[i].url + '"]');
-                el.a.textContent = group.tabs[i].title;
+        curGroupDiv = groupsDiv.query('.group').only(i); // current group
+        curGroupDiv.add('h2').textContent = 'Created: ' + group.date; // add h2
+        curGroupDiv.each(function (el) { // current div
+            el.add('ul>li*' + group.tabs.length); // add ul and lis
+            el.ul.li.each(function (el, i) { // for each li
+                el.add('a[href="' + group.tabs[i].url + '"]'); // add an anchor
+                el.a.textContent = group.tabs[i].title; // add text to anchor
             });
         });
     });
