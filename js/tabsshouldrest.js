a61c3f63a9d40ffccb263cc2dacef4e197894855
Greduan <eduanlavaque@gmail.com>
2014-10-30 21:36:08 -0600

    Started generating HTML from tab groups
    I also moved to using an event page instead of an event script.  Still
    haven't quite figured this out yet, but it's coming along nicely.

--- js/tabsshouldrest.js
+++ js/tabsshouldrest.js
@@ -1,18 +1,73 @@
-/*global chrome, HTML */
+/*global chrome, localStorage, HTML */
 
 (function (H) {
     'use strict';
 
-    // H.query("#container #groups");
+    var groupsDiv = H.query("#groups", 1),
+        curGroupDiv;
 
-    function getTabGroups() {
-        chrome.runtime.sendMessage({ action: 'gettabgroups' }, function (res) {
-            console.log('got response');
-            console.log(res.tabGroups);
+    // from the array of Tab objects it makes an object with date and the array
+    function makeTabGroup(tabsArr) {
+        var tabsInfo = [],
+            tabGroup = { groupDate: new Date() };
+
+        tabsArr.forEach(function (tab) {
+            tabsInfo.push({
+                tabTitle: tab.title,
+                tabUrl: tab.url
+            });
         });
+
+        tabGroup.tabs = tabsInfo;
+
+        return tabGroup;
     }
 
-    console.log('getting tab groups');
-    getTabGroups();
+    // filters tabGroup for stuff like pinned tabs, chrome:// tabs etc.
+    function filterTabGroup(tabGroup) {
+        return tabGroup;
+    }
+
+    // saves array (of Tab objects) to localStorage
+    function saveTabGroup(tabGroup) {
+        if (!(localStorage.tabGroups === undefined) && Array.isArray(JSON.parse(localStorage.tabGroups))) {
+            var parsedTabGroups = JSON.parse(localStorage.tabGroups);
+            parsedTabGroups.push(tabGroup);
+            localStorage.setItem('tabGroups', JSON.stringify(parsedTabGroups));
+        } else {
+            localStorage.setItem('tabGroups', JSON.stringify([ tabGroup ]));
+        }
+    }
+
+    // makes a tab group, filters it and saves it to localStorage
+    function ripTabs(tabsArr) {
+        var tabGroup = makeTabGroup(tabsArr),
+            cleanTabGroup = filterTabGroup(tabGroup);
+
+        saveTabGroup(cleanTabGroup);
+    }
+
+    // listen for when the browser action is clicked
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        chrome.tabs.query({ currentWindow: true }, function (tabsArr) {
+            ripTabs(tabsArr); // store current tab group as a new tab group
+            // open the background page with the groups list
+            chrome.tabs.create({ url: chrome.extension.getURL('tabsshouldrest.html') });
+        });
+    });
+
+    // make HTML with the groups
+    JSON.parse(localStorage.tabGroups).forEach(function (group, index) {
+        groupsDiv.add("div.group");
+
+        curGroupDiv = groupsDiv.query(".group").only(index);
+        curGroupDiv.add("h2").textContent = group.groupDate;
+        curGroupDiv.each(function (el) {
+            el.add("ul>li*" + group.tabs.length);
+            el.ul.li.each(function (el, i) {
+                el.textContent = group.tabs[i].tabTitle;
+            });
+        });
+    });
 
 }(HTML));
