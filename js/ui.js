d2d975c0968fa2be5903e2b8f0cf208c039bc5a4
Greduan <eduanlavaque@gmail.com>
2014-11-02 10:47:33 -0600

    Add some tab ID stuff to .delete-link

--- js/ui.js
+++ js/ui.js
@@ -16,12 +16,18 @@ document.addEventListener('DOMContentLoaded', (function (H) {
             curGroupDiv.each(function (el) { // current div
                 el.add('ul>li*' + group.tabs.length); // add ul and lis
                 el.ul.li.each(function (el, i) { // for each li
-                    el.add('div.delete-link');
+                    el.add('div.delete-link[data-tab-id=' + group.tabs[i].id + ']');
                     el.add('a[href="' + group.tabs[i].url + '"]'); // add an anchor
                     el.a.textContent = group.tabs[i].title; // add text to anchor
                 });
             });
         });
     }
+
+    H.query('.delete-link').each(function (el) {
+        el.addEventListener('click', function () {
+            console.log(el.getAttribute('data-tab-id'));
+        });
+    });
 
 }(HTML)));
