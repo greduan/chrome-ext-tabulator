342367f9640e9b64aa99a31d44f0ab560e685948
greduan <me@greduan.com>
2019-01-01 21:00:31 +0100

    Run pretty-quick on the files

--- options.js
+++ options.js
@@ -1,69 +1,93 @@
-;(function ($) {
-	'use strict';
+(function($) {
+  'use strict';
 
-	var opts = {};
+  var opts = {};
 
-	document.addEventListener('DOMContentLoaded', function () {
-		chrome.storage.local.get('options', function (storage) {
-			var opts = storage.options || {};
+  document.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.local.get('options', function(storage) {
+      var opts = storage.options || {};
 
-			if (opts.deleteTabOnOpen === undefined) {
-				$('input[name="deleteTabOnOpen"][value="no"]').prop('checked', 'checked');
-			} else {
-				$('input[name="deleteTabOnOpen"][value="' + opts.deleteTabOnOpen + '"]').prop('checked', 'checked');
-			}
+      if (opts.deleteTabOnOpen === undefined) {
+        $('input[name="deleteTabOnOpen"][value="no"]').prop(
+          'checked',
+          'checked'
+        );
+      } else {
+        $(
+          'input[name="deleteTabOnOpen"][value="' + opts.deleteTabOnOpen + '"]'
+        ).prop('checked', 'checked');
+      }
 
-			if (opts.closePinnedTabs === undefined) {
-				$('input[name="closePinnedTabs"][value="no"]').prop('checked', 'checked');
-			} else {
-				$('input[name="closePinnedTabs"][value="' + opts.closePinnedTabs + '"]').prop('checked', 'checked');
-			}
-		});
-	});
+      if (opts.closePinnedTabs === undefined) {
+        $('input[name="closePinnedTabs"][value="no"]').prop(
+          'checked',
+          'checked'
+        );
+      } else {
+        $(
+          'input[name="closePinnedTabs"][value="' + opts.closePinnedTabs + '"]'
+        ).prop('checked', 'checked');
+      }
+    });
+  });
 
-	document.getElementById('import').addEventListener('click', () => {
-		const newGroups = document.getElementById('import-text').value.split('\n\n').map((block, i) => ({
-			tabs: block.split('\n').map(item => {
-				const [url, ...rest] = item.split('|')
-				return {url: url.trim(), title: rest.join('|').trim(), pinned: false}
-			}),
-			id: Date.now() - i,
-			date: Date.now(),
-		}));
-        // console.log(newGroups)
-        chrome.storage.local.get('tabGroups', storage => {
-            const groups = storage.tabGroups || [];
-            chrome.storage.local.set({
-                    tabGroups: newGroups.concat(groups)
-                },
+  document.getElementById('import').addEventListener('click', () => {
+    const newGroups = document
+      .getElementById('import-text')
+      .value.split('\n\n')
+      .map((block, i) => ({
+        tabs: block.split('\n').map(item => {
+          const [url, ...rest] = item.split('|');
+          return {
+            url: url.trim(),
+            title: rest.join('|').trim(),
+            pinned: false,
+          };
+        }),
+        id: Date.now() - i,
+        date: Date.now(),
+      }));
+    // console.log(newGroups)
+    chrome.storage.local.get('tabGroups', storage => {
+      const groups = storage.tabGroups || [];
+      chrome.storage.local.set(
+        {
+          tabGroups: newGroups.concat(groups),
+        },
 
+        function() {
+          // show "Import successful" notice thing
+          document.getElementById('imported').style.display = 'block';
+          window.setTimeout(function() {
+            document.getElementById('imported').style.display = 'none';
+          }, 2000);
+        }
+      );
+    });
+  });
 
-                function() { // show "Import successful" notice thing
-                    document.getElementById('imported').style.display = 'block';
-                    window.setTimeout(function() {
-                        document.getElementById('imported').style.display = 'none';
-                    }, 2000);
-                }
+  document.getElementsByName('save')[0].addEventListener('click', function() {
+    var deleteTabOnOpen = document.querySelector(
+      'input[name="deleteTabOnOpen"]:checked'
+    ).value;
+    var closePinnedTabs = document.querySelector(
+      'input[name="closePinnedTabs"]:checked'
+    ).value;
 
-            )
-        })
-    })
-
-	document.getElementsByName('save')[0].addEventListener('click', function () {
-		var deleteTabOnOpen = document.querySelector('input[name="deleteTabOnOpen"]:checked').value;
-		var closePinnedTabs = document.querySelector('input[name="closePinnedTabs"]:checked').value;
-
-		chrome.storage.local.set({
-			options: {
-				deleteTabOnOpen: deleteTabOnOpen,
-				closePinnedTabs: closePinnedTabs
-			}
-		}, function () { // show "settings saved" notice thing
-		document.getElementById('saved').style.display = 'block';
-		window.setTimeout(function () {
-			document.getElementById('saved').style.display = 'none';
-		}, 2000);
-	});
-});
-
-}(jQuery));
+    chrome.storage.local.set(
+      {
+        options: {
+          deleteTabOnOpen: deleteTabOnOpen,
+          closePinnedTabs: closePinnedTabs,
+        },
+      },
+      function() {
+        // show "settings saved" notice thing
+        document.getElementById('saved').style.display = 'block';
+        window.setTimeout(function() {
+          document.getElementById('saved').style.display = 'none';
+        }, 2000);
+      }
+    );
+  });
+})(jQuery);
--- popup.js
+++ popup.js
@@ -1,36 +1,44 @@
-;(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    document.getElementById('save-all-but-active').addEventListener('click', function () {
-        chrome.runtime.sendMessage({ action: 'saveAllButActive' }, function (res) {
-            window.close()
-        });
+  document
+    .getElementById('save-all-but-active')
+    .addEventListener('click', function() {
+      chrome.runtime.sendMessage({ action: 'saveAllButActive' }, function(res) {
+        window.close();
+      });
     });
 
-    // all tabs
-    document.getElementById('save-all').addEventListener('click', function () {
-        chrome.runtime.sendMessage({ action: 'saveAll' }, function (res) {
-            window.close()
-        });
+  // all tabs
+  document.getElementById('save-all').addEventListener('click', function() {
+    chrome.runtime.sendMessage({ action: 'saveAll' }, function(res) {
+      window.close();
+    });
+  });
+
+  // open background page
+  document
+    .getElementById('open-background-page')
+    .addEventListener('click', function() {
+      chrome.runtime.sendMessage({ action: 'openbackgroundpage' }, function(
+        res
+      ) {
+        if (res === 'ok') {
+          window.close();
+        }
+      });
     });
 
-    // open background page
-    document.getElementById('open-background-page').addEventListener('click', function () {
-        chrome.runtime.sendMessage({ action: 'openbackgroundpage' }, function (res) {
-            if (res === 'ok') {
-                window.close();
-            }
-        });
+  document.getElementById('save-active').addEventListener('click', function() {
+    chrome.tabs.query({ currentWindow: true }, function(tabsArr) {
+      chrome.runtime.sendMessage(
+        { action: 'saveActive', tabsArr: tabsArr },
+        function(res) {
+          if (res === 'ok') {
+            window.close();
+          }
+        }
+      );
     });
-
-    document.getElementById('save-active').addEventListener('click', function () {
-        chrome.tabs.query({ currentWindow: true }, function (tabsArr) {
-            chrome.runtime.sendMessage({ action: 'saveActive', tabsArr: tabsArr }, function (res) {
-                if (res === 'ok') {
-                    window.close();
-                }
-            });
-        });
-    });
-
-}());
+  });
+})();
